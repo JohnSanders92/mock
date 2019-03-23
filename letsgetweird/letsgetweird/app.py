538bee6561794926e55e8928e0a4fbdd00fdd43d
John Sanders <jbsanders92@gmail.com>
2019-03-23 03:43:19 -0600

    just reposts data sent from post

--- letsgetweird/letsgetweird/app.py
+++ letsgetweird/letsgetweird/app.py
@@ -1,14 +1,15 @@
 import falcon
-
+import json
 
 class LetsGetWeird(object):
 
     def on_get(self, req, resp):
         print('get')
 
-    def on_post(self,req, resp):
-        print('post')
+    def on_post(self, req, resp):
+        data = json.loads(req.stream.read())
 
+        resp.body = json.dumps(data)
 
 api = application = falcon.API()
 api.add_route('/mock', LetsGetWeird())
