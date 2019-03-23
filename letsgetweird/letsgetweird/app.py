93239e1afcb140035493bc17f8c7c24d59989fa2
John Sanders <jbsanders92@gmail.com>
2019-03-23 14:39:24 -0600

    Update app.py

--- letsgetweird/letsgetweird/app.py
+++ letsgetweird/letsgetweird/app.py
@@ -9,11 +9,7 @@ class LetsGetWeird(object):
     def on_post(self, req, resp):
         data = json.loads(req.stream.read())
         # message = data['message']['text']
-        response = {
-            "text": "I've been hacked",
-            "response_type": "in_channel"
-        }
-        resp.body = json.dumps(response)
+        resp.body = json.dumps(data)
 
 api = application = falcon.API()
 api.add_route('/mock', LetsGetWeird())
