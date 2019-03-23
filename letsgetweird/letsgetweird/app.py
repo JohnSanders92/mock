d748ded98d94e1f991840c4c774d891b8023c855
John Sanders <jbsanders92@gmail.com>
2019-03-23 15:29:07 -0600

    Update app.py

--- letsgetweird/letsgetweird/app.py
+++ letsgetweird/letsgetweird/app.py
@@ -11,16 +11,8 @@ class LetsGetWeird(object):
     def on_post(self, req, resp):
         data = json.loads(req.stream.read())
         # message = data['message']['text']
-        # resp.body = json.dumps(data)
-        
-        slack_token = os.environ["xoxp-304305798082-304290752420-585706548595-acee226ba455c89cdd3a54a514af7724"]
-        sc = SlackClient(slack_token)
+        resp.body = json.dumps(data)
 
-        sc.api_call(
-          "chat.postMessage",
-          channel="shithole",
-          text="Hello from Python! :tada:"
-        )
 
 
 api = application = falcon.API()
