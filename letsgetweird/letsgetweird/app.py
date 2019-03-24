49e33a49db40573ae6c47a5fd0847eded4a871fe
John Sanders <jbsanders92@gmail.com>
2019-03-23 20:15:45 -0600

    Update app.py

--- letsgetweird/letsgetweird/app.py
+++ letsgetweird/letsgetweird/app.py
@@ -11,14 +11,15 @@ class LetsGetWeird(object):
         print('get')
 
     def on_post(self, req, resp):
-
+        data = json.loads(req.stream.read())
         slack_token = os.environ["SLACK_AUTH_KEY"]
         sc = SlackClient(slack_token)
         sc.api_call("chat.postMessage",
                     channel="shithole",
-                    text="Go fuck yourself",
+                    text=data,
                     username="Francisco Duran"
         )
+        
 
 api = application = falcon.API()
 api.add_route('/mock', LetsGetWeird())
