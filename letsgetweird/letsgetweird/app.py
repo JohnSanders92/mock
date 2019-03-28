018c2092d59da34ea4a7768f7a77adb0f08d2993
John Sanders <jbsanders92@gmail.com>
2019-03-28 01:20:27 -0600

    Update app.py

--- letsgetweird/letsgetweird/app.py
+++ letsgetweird/letsgetweird/app.py
@@ -36,7 +36,7 @@ class LetsGetWeird(object):
             output = LetsGetWeird.mockInput(body['message']['text'])
             sc.api_call("chat.postMessage",
                         channel=body['channel']['id'],
-                        text="<@U8Y8JN4Cc",
+                        text="<@U8Y8JN4Cc>",
                         as_user=False,
                         username="mock"
             )
